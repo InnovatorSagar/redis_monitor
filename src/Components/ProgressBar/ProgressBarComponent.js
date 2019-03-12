d9653b29d501102ea71ed7a5af0f0811ec1e8fed
sagar.tyagi <sagar.tyagi@hashedin.in>
2019-03-13 02:25:11 +0530

    Removed merge conflicts

--- src/Components/ProgressBar/ProgressBarComponent.js
+++ src/Components/ProgressBar/ProgressBarComponent.js
@@ -3,18 +3,11 @@ import Filler from "../FillerComponent/FillerComponent";
 import "./ProgressBarComponent.css";
 
 class ProgressBarComponent extends Component {
-    render() {
-        const { percentage } = this.props;
-        let percentag = percentage;
-        if(percentag > 100) {
-            percentag = 100;
-        }
-        let percent = 460*(percentag/100);
-        return (
-            <div className="progress-bar">
-              <Filler percentage={percent} />
-            </div>
-        )
+  render() {
+    const { percentage } = this.props;
+    let percentag = percentage;
+    if (percentag > 100) {
+      percentag = 100;
     }
     let percent = 460 * (percentag / 100);
     return (
