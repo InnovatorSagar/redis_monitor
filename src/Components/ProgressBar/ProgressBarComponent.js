ef303ff3ba7ee131cc68aac98655720ca5dacd81
sagar.tyagi <sagar.tyagi@hashedin.in>
2019-03-15 00:27:46 +0530

    FIxed progress bar component

--- src/Components/ProgressBar/ProgressBarComponent.js
+++ src/Components/ProgressBar/ProgressBarComponent.js
@@ -3,35 +3,27 @@ import Filler from "../FillerComponent/FillerComponent";
 import "./ProgressBarComponent.css";
 
 class ProgressBarComponent extends Component {
-    constructor(props) {
-        super(props);
-            this.state = {
-                lessthan80: true,
-                morethan80: false
-            };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      lessthan80: true,
+      morethan80: false
+    };
+  }
 
-    render() {
-        const { percentage } = this.props;
-        let percentag = percentage;
-        if(percentag > 80 && percentag <= 100) {
-            this.setState({ morethan80: true});
-        }
-        else if(percentag > 100) {
-            percentag = 100;
-        }
-        let percent = 460*(percentag/100);
-        return (
-            <div className="progress-bar">
-              {this.state.lessthan80 && <Filler percentage={percent} />}
-              {this.state.morethan80 && <Filler percentage={percent} />}
-            </div>
-        )
+  render() {
+    const { percentage } = this.props;
+    let percentag = percentage;
+    if (percentag > 80 && percentag <= 100) {
+      this.setState({ morethan80: true });
+    } else if (percentag > 100) {
+      percentag = 100;
     }
     let percent = 460 * (percentag / 100);
     return (
       <div className="progress-bar">
-        <Filler percentage={percent} />
+        {this.state.lessthan80 && <Filler percentage={percent} />}
+        {this.state.morethan80 && <Filler percentage={percent} />}
       </div>
     );
   }
